86223b224e736cddde91aff56e3af196a28a0458
Yannick fumukani <slashcs7@hotmail.com>
2023-05-31 05:00:10 -0500

    ip changed

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -1,3 +1,5 @@
 import axios from 'axios'
 
-export const axiosHttpClient = axios.create({baseURL: 'http://localhost:5600/api/v1'}) ;
+// export const axiosHttpClient = axios.create({ baseURL: 'http://localhost:5600/api/v1' });
+
+export const axiosHttpClient = axios.create({baseURL: 'http://54.197.11.254/api/v1'}) ;
