cfc32fc697f00440d37cd282e7e4a8b59454ea90
Yannick fumukani <slashcs7@hotmail.com>
2023-06-05 04:19:02 -0500

    Switch to production

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -3,4 +3,4 @@ import axios from 'axios';
 const production = `https://slashbk.shop/api/v1`;
 const local = `http://localhost:5600/api/v1`;
 
-export const axiosHttpClient = axios.create({baseURL: local}) ;
+export const axiosHttpClient = axios.create({baseURL: production}) ;
